9f9714949104671ac3e4ecd676fa3dca46c30633
EzwanDeriz <ezwanpderiz@gmail.com>
2026-05-28 17:42:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import math
-
+from streamlit_option_menu import option_menu
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
