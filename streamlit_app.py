b3a9866727582b495cc0c16987ef4f7af1a3f609
EzwanDeriz <ezwanpderiz@gmail.com>
2026-05-30 15:38:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,10 +123,6 @@ if st.button("Hitung Kadar", key = "T1"):
     st.write("Kadar Besi = ", round(kadar_analit, 4))
     st.success(f"Kadar Besi adalah {kadar_analit:.2f}%")
 
-
-bobot_primer = st.number_input("Masukkan bobot baku primer (mg)")
-fp =  st.number_input("Masukkan faktor pengali") 
-vol_titran =  st.number_input("Masukkan volume titran (mL)")
 be = {
     "berat_ekivalen Asam Oksalat": 63,
     "berat_ekivalen Boraks": 190,
@@ -137,7 +133,16 @@ selected_be = st.selectbox(
 berat_ekivalen = be[selected_be]
 st.write("Berat Ekivalen = ", berat_ekivalen, "mg/mgrek")
 
+bobot_primer = st.number_input("Masukkan bobot baku primer (mg): ")
+fp =  st.number_input("Masukkan faktor pengali: ") 
+vol_titran =  st.number_input("Masukkan volume titran (mL): ")
+st.write("Bobot baku primer: ", round(bobot_primer, 4), "mg")
+st.write("Faktor pengenceran: ", fp)
+st.write("Volume titran: "), round(vol_titran, 2), "mL")
+
+
 if st.button("Hitung Normalitas", key = "G1"):
     normalitas = standarisasi_asam_basa(bobot_primer, fp, vol_titran, berat_ekivalen)
     st.write("Normalitas = ", round(normalitas, 4))
     st.success(f"Normalitas adalah {round(normalitas, 4)} mgrek/mL")
+
