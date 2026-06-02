e3369eb6eed15e1ab5b194600ac1f4917821e347
EzwanDeriz <ezwanpderiz@gmail.com>
2026-06-02 18:41:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -227,7 +227,7 @@ elif menu == "Gravimetri":
         selected_fg = st.selectbox(
             "Pilih Faktor Gravimetri", list(fg.keys()), key = ("S4"))
         faktor_gravi = fg[selected_fg]
-        st.write("Faktor Gravimetri = ", round(faktor_gravi, 4))
+        st.write("Faktor Gravimetri = ", faktor_gravi, format="%.4f")
         
         bobot_analit = st.number_input(
             "Masukkan bobot analit (g): ",
