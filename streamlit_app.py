be56b41d117da5b8c0ea70ccb2ed6d379c5aee68
EzwanDeriz <ezwanpderiz@gmail.com>
2026-06-02 19:27:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -240,8 +240,8 @@ elif menu == "Gravimetri":
         
         if st.button("Hitung Kadar", key="T3"):
             kadar_analit = perhitungan_kadar_bperb(faktor_gravi, bobot_analit, bobot_sampel)
-            st.write(f"Kadar Besi = {kadar_analit:.2f}")
-            st.success(f"Kadar Besi adalah {kadar_analit:.2f}%")
+            st.write(f"Kadar Sulfat = {kadar_analit:.2f}", "%")
+            st.success(f"Kadar Sulfat adalah {kadar_analit:.2f}%")
             
     with tab4:
         st.write("Kadar Besi")
@@ -274,7 +274,7 @@ elif menu == "Gravimetri":
         
         if st.button("Hitung Kadar", key = "T4"):
             kadar_analit = perhitungan_kadar_bperb(faktor_gravi, bobot_analit, bobot_sampel)
-            st.write("Kadar Besi = ", round(kadar_analit, 4))
+            st.write("Kadar Besi = {kadar_analit:.2f}", "%")
             st.success(f"Kadar Besi adalah {kadar_analit:.2f}%")
             
     with tab5:
