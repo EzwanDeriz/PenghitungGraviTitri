14831f7a61cd4aef405d28fe939dc530231cb3ac
EzwanDeriz <ezwanpderiz@gmail.com>
2026-06-15 19:46:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -901,7 +901,7 @@ elif menu == "% RPD":
             format="%.4f",
             key="N2")
     rerata = (nilai1 + nilai2) / 2
-    rpd = abs((nilai1 - nilai2) / rerata) * 100
+    rpd = abs({nilai1 - nilai2} / rerata) * 100
     if st.button("Hitung %RPD", key = "RPD"):
         st.success(f"%RPD = ((|{nilai1} - {nilai2}|) / {rerata}) x 100%")
         st.success(f"Nilai % RPD didapatkan sebesar: {rpd:.2f} %")
