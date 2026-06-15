bb108b975d7a76588a5e2fe5820b867a34162601
EzwanDeriz <ezwanpderiz@gmail.com>
2026-06-15 12:43:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -307,6 +307,8 @@ elif menu == "Gravimetri":
     st.markdown("---")
     tab1, tab2, tab3, tab4, tab5, tabX = st.tabs(["Kadar Air", "Kadar Abu", "Kadar Sulfat", "Kadar Besi", "Kadar Ba", "Custom"])
     with tab1:
+        with tengah:
+            st.image:("air.jpeg")
         # Input bobot analit dengan 4 angka belakang koma
         bobot_analit = st.number_input(
             "Masukkan bobot analit (g): ",
